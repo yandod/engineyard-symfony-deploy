8ddadd19a4e69d733fcc69e271acf1307e195869
Yusuke Ando <yandod@gmail.com>
2013-02-06 18:55:08 +0900

    up

--- before_bundle.rb
+++ before_bundle.rb
@@ -1,3 +1,4 @@
 run "curl -s https://getcomposer.org/installer | php -d allow_url_fopen=on"
+run "cp #{release_path}/app/config/parameters.yml.example #{release_path}/app/config/parameters.yml"
 run "php -d allow_url_fopen=on composer.phar install"
 run "sed 's/root \/data\/symfony_app\/current/\/data\/symfony_app\/current/web' > /etc/nginx/servers/symfony_app.conf > /etc/nginx/servers/symfony_app.conf.tmp; cp /etc/nginx/servers/symfony_app.conf.tmp /etc/nginx/servers/symfony_app.conf''"
