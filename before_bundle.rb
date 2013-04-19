c1e6e0910a3e6341650dae720f6a0b9db3f63ea0
Yusuke Ando <yandod@gmail.com>
2013-04-19 10:25:34 +0900

    Update before_bundle.rb

--- before_bundle.rb
+++ before_bundle.rb
@@ -3,14 +3,14 @@ run "cp #{release_path}/app/config/parameters.yml.example #{release_path}/app/co
 run "ln -s #{release_path}/web/app.php #{release_path}/web/index.php"
 
 # set timezon in php.ini
-sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cgi-php5.3/ext-active/timezone.ini"
-sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cli-php5.3/ext-active/timezone.ini"
-sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/fpm-php5.3/ext-active/timezone.ini"
+sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cgi-php5.4/ext-active/timezone.ini"
+sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cli-php5.4/ext-active/timezone.ini"
+sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/fpm-php5.4/ext-active/timezone.ini"
 
 # kick composer install
 run "curl -s https://getcomposer.org/installer | php -d allow_url_fopen=on"
 run "php -d allow_url_fopen=on composer.phar install"
 
 # change nginx document root for Symfony
-run 'sed  -e \'s/root \/data\/symfony_app\/current;/root \/data\/symfony_app\/current\/web;/\' /etc/nginx/servers/symfony_app.conf > /etc/nginx/servers/symfony_app.conf.tmp'
-run "cp /etc/nginx/servers/symfony_app.conf.tmp /etc/nginx/servers/symfony_app.conf"
+# run 'sed  -e \'s/root \/data\/symfony_app\/current;/root \/data\/symfony_app\/current\/web;/\' /etc/nginx/servers/symfony_app.conf > /etc/nginx/servers/symfony_app.conf.tmp'
+# run "cp /etc/nginx/servers/symfony_app.conf.tmp /etc/nginx/servers/symfony_app.conf"
